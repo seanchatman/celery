459d533b84063014c19b056b104b2263495da930
Sean Chatman <sean_chatman@intuit.com>
2023-05-31 12:13:49 -0700

    Fix key

--- openai_operations.py
+++ openai_operations.py
@@ -4,7 +4,7 @@ from time import strftime, gmtime, sleep
 
 import openai
 
-openai.api_key = os.environ["OPENAI_API_KEY"]
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
 def gpt3_completion(prompt, engine='text-davinci-003', temp=1.0, top_p=1.0, tokens=400, freq_pen=0.0, pres_pen=0.0,
